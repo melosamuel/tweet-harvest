1ab55f7a17209aea0f7e0a407b7716cfc1220e7a
melosamuel <merlosamuel@protonmail.ch>
2024-06-21 21:33:08 -0300

    test can browse

--- src/test/test_case.py
+++ src/test/test_case.py
@@ -9,6 +9,7 @@ def start_browser():
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=800,600")
+    options.add_argument("--log-level=3")
 
     driver = webdriver.Chrome(options=options)
 
@@ -20,3 +21,39 @@ def test_launch_browser(start_browser):
     browser, wait = start_browser
 
     assert browser != None
+
+def test_can_browse(start_browser):
+    browser, wait = start_browser
+    url = "https://www.google.com/"
+
+    browser.get(url)
+
+    assert browser.current_url == url
+
+@pytest.mark.skip(reason="Not implemented yet!")
+def test_login():
+    pass
+
+@pytest.mark.skip(reason="Not implemented yet!")
+def test_can_find_a_tweet():
+    pass
+
+@pytest.mark.skip(reason="Not implemented yet!")
+def test_exctract_username():
+    pass
+
+@pytest.mark.skip(reason="Not implemented yet!")
+def test_exctract_tweet_text():
+    pass
+
+@pytest.mark.skip(reason="Not implemented yet!")
+def test_exctrat_numbers():
+    pass
+
+@pytest.mark.skip(reason="Not implemented yet!")
+def test_can_find_replies():
+    pass
+
+@pytest.mark.skip(reason="Not implemented yet!")
+def test_exctract_tweet_url():
+    pass
